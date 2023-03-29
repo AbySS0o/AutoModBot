7dd92a110476f7127c6192c196d2ab45a28ca4ec
Egor <abyss5006@gmail.com>
2023-03-29 21:57:07 +0300

    everything

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import itertools
 from aiogram import Bot, Dispatcher, executor, types
 import json
 import string
+from os import path
 import pymorphy2
 import os
 
@@ -11,6 +12,7 @@ from aiogram.dispatcher.handler import CancelHandler
 from aiogram.dispatcher.middlewares import BaseMiddleware
 from aiogram.types import MessageEntityType
 
+#логирование, конфиг
 logging.basicConfig(level=logging.INFO)
 bot = Bot('6140941878:AAGrkh469KVD1OZWsPe9PZIMtiqckBFGLXg')
 dp = Dispatcher(bot)
@@ -19,47 +21,52 @@ async def on_startup(_):
     print('Bot has been connected')
 
 def normal_mat(word:str):
+    #делаем все нижним регистром (строчным), убираем все знаки ^$#^&**^$@#&*|/. и тд
     word = word.lower().translate(str.maketrans('','',string.punctuation))
+    #убираем все повторяющиеся буквы
+    word = ''.join(i for i, _ in itertools.groupby(word))
+    #склоняем по падежам/числам, все вариации мата крч
     morph = pymorphy2.MorphAnalyzer()
     parsed_token = morph.parse(word)
     #выводится нормальная форма. normal_form см. выше
     normal_form = parsed_token[0].normal_form
-    #делаем проверку на повторяющиеся символы и удаляем их
-    r = ''.join(i for i, _ in itertools.groupby(normal_form))
-    return r
+    #возвращаем результат всех процедур
+    print(normal_form)
+    return normal_form
 
 @dp.message_handler()
 async def echo_send(message : types.Message):
-    admins_list = [admin.user.id for admin in await bot.get_chat_administrators(chat_id=message.chat.id)]
-    if message.from_user.id not in admins_list:
-        if {normal_mat(i) for i in message.text.split(' ')}.intersection(set(json.load(open('E:\pythonProject\oigram bot\cenz.json')))) != set():  
-            await message.reply('Погане повідомлення')
-            await bot.delete_message(message.chat.id, message.message_id)
-            await bot.restrict_chat_member(message.chat.id, message.from_user.id, types.ChatPermissions(can_send_messages=False), until_date = datetime.datetime.now() + datetime.timedelta(seconds=30))
-            print('Мут был выдан успешно')
-    else:
-        print('Повідомлення написав адміністратор')
+            #для поиска места директории указанного файла со словарем банвордов cenz.json
+            parent_dir = path.dirname(path.abspath(__file__))
+            #делаем проверку + всё в 1 строчку, разделяя пробелом
+            if {normal_mat(i) for i in message.text.split(' ')}.intersection(set(json.load(open(path.join(parent_dir, 'cenz.json'))))) != set():  
+                await message.reply('Твоє повідомлення містило непристойне слово, тому воно було видалене. Гравець замучений на 5хв')
+                await bot.delete_message(message.chat.id, message.message_id)
+                await bot.restrict_chat_member(message.chat.id, message.from_user.id, types.ChatPermissions(can_send_messages=False), until_date = datetime.datetime.now() + datetime.timedelta(seconds=30))
+                print('Мут был выдан успешно')
 
-@dp.message_handler(content_types=['text'])
-async def delete_messages(message: types.Message):
-    for entity in message.entities:
-        if entity.type in ["url", "text_link", "https"]:
-            
-            await message.delete()
 
 class UrlMiddleWare(BaseMiddleware):
     async def on_pre_process_message(self, message: types.Message, data: dict):
-            if msg_entities := (message.entities or message.caption_entities):
-                for entitie in msg_entities:
-                    if entitie.type in [MessageEntityType.URL, MessageEntityType.TEXT_LINK]:
-                        if message.text != 'https://twitter.com/home?lang=ru':
-                            await message.reply('Посилання видалено')
-                            await message.delete()
-                            print("Посилання видалено")
-                            raise CancelHandler()
-                        else:
-                            print('Не є заблокованим посиланням')
-
+            #проверка на админа чата
+            admins_list = [admin.user.id for admin in await bot.get_chat_administrators(chat_id=message.chat.id)]
+            if message.from_user.id not in admins_list:
+                if msg_entities := (message.entities or message.caption_entities):
+                    for entitie in msg_entities:
+                        if entitie.type in [MessageEntityType.URL, MessageEntityType.TEXT_LINK]:
+                            #удаляем ссылки непохожие на эти
+                            if message.text != 'https://forum.samp-ukraine.com/index.php':
+                                await message.reply('Посилання видалено')
+                                await message.delete()
+                                print("Посилання видалено")
+                                raise CancelHandler()
+                            elif message.text != 'https://www.samp-ukraine.com/':
+                                await message.reply('Посилання видалено')
+                                await message.delete()
+                                print("Посилання видалено")
+                                raise CancelHandler()
+                            else:
+                                print('Не є заблокованим посиланням')
 
 
 
